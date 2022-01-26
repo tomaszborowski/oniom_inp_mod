db628230102678ac489769a1a90a091c03641c1d
tomaszborowski <tomasz.borowski74@gmail.com>
2022-01-26 15:07:29 +0100

    example added

--- oniom_inp_mod.py
+++ oniom_inp_mod.py
@@ -7,5 +7,20 @@ authors: Jakub Baran, Paulina Miśkowiec, Tomasz Borowski
 from oniom_inp_mod_aux import atom, link_atom
 
 
+###     test cases     ###
+oniom_inp_to_read = 'input_examples/h6h-oxo+succinate+water_hyo_17_07_b2.com'
+
+oniom_inp_to_write = 'input_examples/h6h-oxo+succinate+water_hyo_17_07_b2_mod.com'
+pre_resp_qm_inp_to_write = ''
+qm_inp_to_write = ''
+
+resp_qout_file_to_read = 'input_examples/h6h-oxo+succinate+water_hyo_rep3_copy3_clust3-resp2.qout'
+whole_system_xyz_file_to_read = 'input_examples/h6h-oxo+succinate+water_hyo_17_07_b_moved.xyz'
+qm_system_xyz_file_to_read = ''
+
+ee_qm_mm_q_model = 'CS'
+##########################
+
+
 
 
